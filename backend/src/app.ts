6bd6b23a989c076c541d634ab493766af62d85e3
Hemalatha <hemajayarajan05@gmail.com>
2026-01-06 22:53:33 +0530

    Done|routes modification

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
-
+import contactRoutes from "./routes/contact.routes";
+import { contactLimiter } from "./middlewares/ratelimit.middleware";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contact",contactLimiter,contactRoutes);
 
 
 export default app;
